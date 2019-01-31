0123b2843986d82bb494cf584daa320157efc861
fbeji <fbeji@unm.edu>
2019-01-31 16:23:34 -0700

    Unfinished card game

--- src/edu/cnm/deepdive/simpleWar.java
+++ src/edu/cnm/deepdive/simpleWar.java
@@ -4,7 +4,7 @@ import java.security.SecureRandom;
 import java.util.Comparator;
 import java.util.Random;
 
-public class War {
+public class simpleWar {
 
   private Deck deck;
   private Random rng;
@@ -12,7 +12,7 @@ public class War {
   private int tally2;
   private Referree referree;
 
-  public War(Random rng){
+  public simpleWar(Random rng){
 
     this.rng = rng;
     deck = new Deck();
@@ -55,10 +55,49 @@ public class War {
 
   }
 
+  public Deck getDeck() {
+    return deck;
+  }
+
+  public void setDeck(Deck deck) {
+    this.deck = deck;
+  }
+
+  public Random getRng() {
+    return rng;
+  }
+
+  public void setRng(Random rng) {
+    this.rng = rng;
+  }
+
+  public int getTally1() {
+    return tally1;
+  }
+
+  public void setTally1(int tally1) {
+    this.tally1 = tally1;
+  }
+
+  public int getTally2() {
+    return tally2;
+  }
+
+  public void setTally2(int tally2) {
+    this.tally2 = tally2;
+  }
+
+  public Referree getReferree() {
+    return referree;
+  }
+
+  public void setReferree(Referree referree) {
+    this.referree = referree;
+  }
 
   public static void main(String[] args) {
 
-    War war = new War(new SecureRandom());
+    simpleWar war = new simpleWar(new SecureRandom());
     try {
       while(true) {
         war.play();
